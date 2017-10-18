11a5b2fc8b9170f3c4494c07dd51c517406d3eab
Patrick Lindsay <patrick.lindsay@me.com>
2017-10-18 15:49:46 -0500

    Define Event model

--- db/migrate/20171018201610_create_events.rb
+++ db/migrate/20171018201610_create_events.rb
@@ -1,6 +1,11 @@
 class CreateEvents < ActiveRecord::Migration
   def change
     create_table :events do |t|
+      t.string :name
+      t.text :description
+      t.string :location
+      t.datetime :starting_at
+      t.datetime :ending_at
 
       t.timestamps null: false
     end
